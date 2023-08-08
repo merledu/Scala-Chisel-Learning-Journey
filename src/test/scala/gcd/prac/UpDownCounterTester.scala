f59e76240af813e7c3d4bd45a9f020da377ad769
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2023-08-09 01:09:57 +0500

    check commit

--- src/test/scala/gcd/prac/UpDownCounterTester.scala
+++ src/test/scala/gcd/prac/UpDownCounterTester.scala
@@ -5,6 +5,8 @@ import chisel3.tester._
 import org.scalatest.FreeSpec
 import chisel3.experimental.BundleLiterals._
 
+//change karne ki koshish
+
 class UpDownCounterTester extends FreeSpec with ChiselScalatestTester{
     "updownCounter2tester file " in {
         test(new counter_up_down(4)){ a=>
