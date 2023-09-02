3d335951c72e058e3a937842e316f0b06b3598e8
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2023-09-02 22:09:18 +0500

    lab 6 ex 3 done

--- src/main/scala/gcd/lab_6/ex4.scala
+++ src/main/scala/gcd/lab_6/ex4.scala
@@ -1,12 +1,18 @@
-//package lab_6
-//import chisel3 . _
-//import chisel3 . util . _
-//
-//class My_Queue extends Module {
-//  val io = IO(new Bundle{
-//    val in = Decoupled(Input())
-//
-//  } )
-//  // your code begin
-//  // y
-//}
+package lab_6
+import chisel3 . _
+import chisel3 . util . _
+
+class My_Queue extends Module {
+  val io = IO(new Bundle{
+    val out = Decoupled(Input(UInt(8.W)))
+    val in = Flipped(Decoupled(Output(UInt(8.W))))
+  } )
+
+  val que1 = Queue(io.in , 5)
+  val que2 = Queue(que1 , 5)
+
+  //que2 <> que1
+  io.out <> que2
+
+
+}
